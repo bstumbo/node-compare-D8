9fffd5d7514d0c93d9441c0afc08ae640a46482b
Brandon Stumbo <bstumbo@gwu.edu>
2017-09-01 12:17:51 -0500

    Created NodeCompareBlock.php, NodeCompareBlockForm.php, permissions.yml;
    Signed-off-by: Brandon Stumbo <bstumbo@gwu.edu>

---
+++ src/Form/NodeCompareBlockForm.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Drupal\node_compare\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
+
+/**
+ * Lorem Ipsum block form
+ */
+class NodeCompareBlockForm extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'node_compare_block_form';
+  }
+  
+   /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+   
+    $form['node_compare_show_history'] = array(
+      '#type' => 'checkbox',
+      '#title' => t('Show recent comparisons'),
+      '#description' => t('Display the recent comparisons list in the comparison block.'),
+      #'#default_value' => $config->get('node_compare_show_history'),
+    );
+
+    return $form;
+  }
+  
+   /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    
+  
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $form_state->setRedirect(
+      'node_compare.form',
+      array(
+        'node_compare_show_history' => $form_state->getValue('node_compare_show_history'),
+      )
+    );
+  }
+
+}
---
+++ src/Plugin/Block/NodeCompareBlock.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Drupal\node_compare\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Block\BlockPluginInterface;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Provides a Node Compare Block.
+ *
+ * @Block(
+ *   id = "node_compare_items",
+ *   admin_label = @Translation("Node Compare Block"),
+ *   category = @Translation("Node Compare"),
+ * )
+ */
+class NodeCompareBlock extends BlockBase implements BlockPluginInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  
+  public function defaultConfiguration() {
+    $default_config = \Drupal::config('node_compare.settings');
+    return array(
+      'node_compare_name' => $default_config->get('node_compare_block.name'),
+    );
+  }
+  
+ /* protected function blockAccess(AccountInterface $account) {
+    return AccessResult::allowedIfHasPermission($account, 'use comparison');
+  } */
+  
+  public function blockForm($form, FormStateInterface $form_state) {
+    $form = parent::blockForm($form, $form_state);
+
+    $config = $this->getConfiguration();
+
+    $form['node_compare_show_history'] = array(
+      '#type' => 'checkbox',
+      '#title' => t('Show recent comparisons'),
+      '#description' => t('Display the recent comparisons list in the comparison block.'),
+      #'#default_value' => $config->get('node_compare_show_history'),
+    );
+
+    return $form;
+  }
+  
+   public function blockSubmit($form, FormStateInterface $form_state) {
+    $this->configuration['node_compare_show_history'] = $form_state->getValue('node_compare_show_history');
+  }
+  
+  
+  public function build() {
+    return \Drupal::formBuilder()->getForm('Drupal\node_compare\Form\NodeCompareBlockForm');
+    
+   /* $config = $this->getConfiguration();
+    
+    return array(
+    'node_compare_form' => $form,
+    'subject' => t('Content for comparison'),
+    'content' => array(
+      '#theme' => 'node_compare_block_content',
+      '#prefix' => '<div  id="node-compare-items">',
+      '#suffix' => '</div>',
+      ),
+    ); */
+  }
+
+}
